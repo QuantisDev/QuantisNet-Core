3323de65d25310091df503e7310cb0aae866b205
Akshay <akshaycm@hotmail.com>
2019-06-13 23:32:10 +0300

    [Trivial] Update Git and website links in compiles

--- src/init.cpp
+++ src/init.cpp
@@ -626,8 +626,8 @@ std::string HelpMessage(HelpMessageMode mode)
 
 std::string LicenseInfo()
 {
-    const std::string URL_SOURCE_CODE = "<https://github.com/quantisnetcryptocurrency/quantisnet";
-    const std::string URL_WEBSITE = "<https://www.quantisnet.world>";
+    const std::string URL_SOURCE_CODE = "<https://github.com/QuantisDev/QuantisNet-Core-v2.1.2";
+    const std::string URL_WEBSITE = "<https://www.quantisnetwork.org>";
 
     return CopyrightHolders(_("Copyright (C)"), 2017, COPYRIGHT_YEAR) + "\n" +
            "\n" +
